096b1ee251c4aead7d7bf2a45295c17e76a84348
inhyeokjeong <darkages82@nate.com>
2015-07-05 14:35:27 +0900

    Update DarkInputParam.java
    주석 변경

--- dit/dit-test/src/main/java/com/dit/test/restful/model/DarkInputParam.java
+++ dit/dit-test/src/main/java/com/dit/test/restful/model/DarkInputParam.java
@@ -1,10 +1,3 @@
-/* 
- * Copyright (C) 2011 LG CNS Inc.
- * All rights reserved.
- *
- * 모든 권한은 LG CNS(http://www.lgcns.com)에 있으며,
- * LG CNS의 허락없이 소스 및 이진형식으로 재배포, 사용하는 행위를 금지합니다.
- */
 package com.dit.test.restful.model;
 
 import javax.xml.bind.annotation.XmlRootElement;
